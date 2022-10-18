f44bec2d8b3f0ccd989ce4fd2d36f39a08fce799
sallecta <sallecta@inbox.ru>
2022-10-18 21:05:15 +0300

    ...

--- Sources/old jcmvc_04_model_site/site/controller.php
+++
@@ -1,5 +0,0 @@
-<?php
-defined('_JEXEC') or die('Restricted access');
-class JcmvcController extends \\Joomla\\CMS\\MVC\\Controller\\BaseController
-{
-}
--- Sources/old jcmvc_04_model_site/site/views/first/tmpl/default.php
+++
@@ -1,5 +0,0 @@
-<?php
-defined('_JEXEC') or die('Restricted access');
-// $this refers to the current view class
-?>
-<h1><?php echo $this->msg; ?></h1>
